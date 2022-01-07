59a77bfb1444a8aa44e93d331bfac040d356c92d
vmwhoami <vitalie.m@travod.com>
2022-01-07 14:03:19 +0100

    Create images and start containers from them

--- backend/app.js
+++ backend/app.js
@@ -84,7 +84,7 @@ app.delete('/goals/:id', async (req, res) => {
 });
 
 mongoose.connect(
-  'mongodb://localhost:27017/course-goals',
+  'mongodb://mongodb/course-goals',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
