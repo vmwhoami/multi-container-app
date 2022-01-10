4c56406fe095a3306d2e75e6bbf5c7f2a0780205
vmwhoami <vitalie.m@travod.com>
2022-01-10 20:44:04 +0200

    Add authentication

--- backend/app.js
+++ backend/app.js
@@ -84,7 +84,7 @@ app.delete('/goals/:id', async (req, res) => {
 });
 
 mongoose.connect(
-  'mongodb://mongodb/course-goals',
+  'mongodb://vitalie:secret@mongodb/course-goals?authSource=admin',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
