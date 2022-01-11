efa9b9c11a79dc6398d36bce0dd5fdfc005af0a0
vmwhoami <vitalie.m@travod.com>
2022-01-11 16:45:43 +0200

    Add env variables

--- backend/app.js
+++ backend/app.js
@@ -84,7 +84,7 @@ app.delete('/goals/:id', async (req, res) => {
 });
 
 mongoose.connect(
-  'mongodb://vitalie:secret@mongodb/course-goals?authSource=admin',
+  `mongodb://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}}@mongodb/course-goals?authSource=admin`,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
